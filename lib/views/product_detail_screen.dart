8371dea229ac64c00ad0ee91f74c6e068d406441
Robson F <robsonfernando.012@gmail.com>
2020-06-09 16:44:09 -0300

    First Commit

---
+++ lib/views/product_detail_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/products.dart';
+import 'package:shop/providers/counter_provider.dart';
+
+class ProductDetailScreen extends StatefulWidget {
+  @override
+  _ProductDetailScreenState createState() => _ProductDetailScreenState();
+}
+
+class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Product product =
+        ModalRoute.of(context).settings.arguments as Product;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(CounterProvider.of(context).state.value.toString()),
+          RaisedButton(
+              child: Text('+'),
+              onPressed: () {
+                setState(() {
+                  CounterProvider.of(context).state.inc();
+                });
+                
+                print(CounterProvider.of(context).state.value);
+              }),
+          RaisedButton(
+              child: Text('-'),
+              onPressed: () {
+                CounterProvider.of(context).state.dec();
+                print(CounterProvider.of(context).state.value);
+              })
+        ],
+      ),
+    );
+  }
+}
